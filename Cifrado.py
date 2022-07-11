1b7312459ad31c205d5d1b1612ac88ec94ae3def
drcortes <33724363+drcortes@users.noreply.github.com>
2022-07-11 10:34:31 -0400

    Update Cifrado.py

--- Cifrado.py
+++ Cifrado.py
@@ -1,8 +1,8 @@
 import sys
 from cryptography.fernet import Fernet
-#key = Fernet.generate_key()
+#key = Fernet.generate_key() La idea es generar un string que sea: "usuario contraseña" con espacio, y el resultante de esa funcion colocarlo en la funci
 #print(key)
-f = Fernet(b"t26WaPWVK9z0zndajIw9MJTCZBuCfACVVEkZhtbh_tg=")
+f = Fernet(b"ggggggggggggggggggggggggggggggg=")
 #token = f.encrypt(bytes(sys.argv[1],"ascii"))
 #print(token)
 DatoDes=f.decrypt(bytes(sys.argv[1],"ascii"))
